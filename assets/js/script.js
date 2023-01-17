f0dac2f9fbfe0c174163daef2a84c9f89e320c1a
Jacob Murray <jakeskata@gmail.com>
2023-01-17 15:51:27 -0500

    fixed local storage, working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,47 @@ hours.forEach((hour,i) => {
   main.innerHTML += `
     <div class="row time-block ${ rowHour < d.getHours() ? 'past' : rowHour > d.getHours() ? 'future' : 'present' }">
         <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
-        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <textarea class="col-8 col-md-10 description" rows="3" id="${hour}"> </textarea>
         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
           <i class="fas fa-save" aria-hidden="true"></i>
       </button>
     </div>
 
   `;
+
 });
 
+
 function saveNote (event) {
+
   if(event.target.getAttribute("class").includes("saveBtn")){ 
     var clickedbutton = event.target
+    
     var textvalue = clickedbutton.previousElementSibling.value
     console.log(textvalue)
-    localStorage.setItem("text",textvalue)
+
+    var textKey = clickedbutton.previousElementSibling.id
+    console.log(textKey)
+
+    localStorage.setItem(textKey,textvalue)
   }
 
 }
+
+function displayNote(){
+  let textarea = document.querySelectorAll(".description")
+  console.log(textarea)
+  
+  textarea.forEach(function(area){
+    let textareaId = area.id
+    
+    console.log(textareaId)
+
+    area.value = localStorage.getItem(textareaId)
+  })
+
+}
+
 document.addEventListener("click", saveNote)
+
+displayNote()
